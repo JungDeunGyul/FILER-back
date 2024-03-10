b1cbf24a2dce1cc6c411694790aff3ccb30f170a
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-03-10 17:54:39 +0900

    chore: update model imports

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const User = require("../models/User");
+const { User } = require("../models/User");
 const verifyToken = require("../middleware/verifyToken");
 
 router.post("/", verifyToken, async (req, res, next) => {
