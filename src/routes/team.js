67baf5db74199d005396dbb5cff0722784d7d51d
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-03-13 04:01:21 +0900

    feat: add file logic for team members

--- src/routes/team.js
+++ src/routes/team.js
@@ -104,6 +104,82 @@ router.post("/:teamName/createfolder/:userId", async (req, res, next) => {
   }
 });
 
+router.post("/:teamName/createfile/:userId", async (req, res, next) => {
+  try {
+    const { userId, teamName } = req.params;
+    const { fileName } = req.body;
+
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const team = await Team.findOne({ name: teamName });
+    if (!team) {
+      return res.status(404).json({ message: "Team not found" });
+    }
+
+    const isUserInTeam = team.members.some((member) =>
+      member.user.equals(userId),
+    );
+    if (!isUserInTeam) {
+      return res
+        .status(403)
+        .json({ message: "유저가 해당 팀에 속해 있지 않습니다." });
+    }
+
+    const userRoleInTeam = team.members.find((member) =>
+      member.user.equals(userId),
+    ).role;
+
+    if (userRoleInTeam !== "팀장" && userRoleInTeam !== "팀원") {
+      return res.status(403).json({ message: "파일을 넣을 권한이 없습니다." });
+    }
+
+    const isFolder = team.ownedFiles.some((file) => file.name === fileName);
+
+    if (isFolder) {
+      return res.status(412).json({ message: "파일 이름이 이미 존재합니다" });
+    }
+
+    const newFile = await File.create({
+      name: fileName,
+      ownerTeam: team._id,
+    });
+
+    team.ownedFiles.push(newFile);
+
+    await team.save();
+    await user.save();
+
+    const updatedUser = await User.findOne({ _id: userId })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "members.user",
+        },
+      })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "ownedFolders",
+        },
+      })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "ownedFiles",
+        },
+      });
+
+    return res
+      .status(201)
+      .json({ message: "파일이 업로드 되었습니다!", updatedUser });
+  } catch (error) {
+    return res.status(400).json({ message: "Faild, create Folder" });
+  }
+});
+
 router.patch("/:teamName/joinrequest/:userId", async (req, res, next) => {
   try {
     const { userId, teamName } = req.params;
@@ -223,7 +299,7 @@ router.post("/:teamName/new/:userId", async (req, res, next) => {
 
     await user.save();
 
-    const updatedUser = await User.findOne({ email: userData.email })
+    const updatedUser = await User.findOne({ _id: userId })
       .populate({
         path: "teams",
         populate: {
