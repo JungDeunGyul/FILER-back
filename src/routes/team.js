7ef64191030a842bc63c5aff7f6a95ec4dcbbf99
JungDeunGyul <ask7384@gmail.com>
2024-03-12 19:31:31 +0900

    Feat: add folder creation logic for team members

--- src/routes/team.js
+++ src/routes/team.js
@@ -5,6 +5,8 @@ const removeJoinRequest = require("../utils/removeJoinReqest");
 
 const { User } = require("../models/User");
 const { Team } = require("../models/Team");
+const { Folder } = require("../models/Folder");
+const { File } = require("../models/File");
 
 let clientTeamJoinRequestSSE = [];
 
@@ -21,6 +23,86 @@ const sendUserDataToClients = (targetUserData, messageTargetUserId, action) => {
     client.write(`data: ${JSON.stringify(message)}\n\n`);
   }
 };
+
+router.post("/:teamName/createfolder/:userId", async (req, res, next) => {
+  try {
+    const { userId, teamName } = req.params;
+    const { folderName } = req.body;
+
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const team = await Team.findOne({ name: teamName });
+    if (!team) {
+      return res.status(404).json({ message: "Team not found" });
+    }
+
+    const isUserInTeam = team.members.some((member) =>
+      member.user.equals(userId),
+    );
+    if (!isUserInTeam) {
+      return res
+        .status(403)
+        .json({ message: "유저가 해당 팀에 속해 있지 않습니다." });
+    }
+
+    const userRoleInTeam = team.members.find((member) =>
+      member.user.equals(userId),
+    ).role;
+
+    if (userRoleInTeam !== "팀장" && userRoleInTeam !== "팀원") {
+      return res
+        .status(403)
+        .json({ message: "폴더를 생성할 권한이 없습니다." });
+    }
+
+    const isFolder = team.ownedFolders.some(
+      (folder) => folder.name === folderName,
+    );
+
+    if (isFolder) {
+      return res.status(412).json({ message: "폴더 이름이 이미 존재합니다" });
+    }
+
+    const newFolder = await Folder.create({
+      name: folderName,
+      ownerTeam: team._id,
+    });
+
+    team.ownedFolders.push(newFolder);
+
+    await team.save();
+    await user.save();
+
+    const updatedUser = await User.findOne({ _id: userId })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "members.user",
+        },
+      })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "members.user",
+        },
+      })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "ownedFolders",
+        },
+      });
+
+    return res
+      .status(201)
+      .json({ message: "Folder created successfully", updatedUser });
+  } catch (error) {
+    return res.status(400).json({ message: "Faild, create Folder" });
+  }
+});
 
 router.patch("/:teamName/joinrequest/:userId", async (req, res, next) => {
   try {
@@ -141,9 +223,29 @@ router.post("/:teamName/new/:userId", async (req, res, next) => {
 
     await user.save();
 
+    const updatedUser = await User.findOne({ email: userData.email })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "members.user",
+        },
+      })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "ownedFolders",
+        },
+      })
+      .populate({
+        path: "teams",
+        populate: {
+          path: "ownedFiles",
+        },
+      });
+
     return res
       .status(201)
-      .json({ message: "Team created successfully", team: newTeam });
+      .json({ message: "Team created successfully", updatedUser });
   } catch (error) {
     return res.status(400).json({ message: "Failed to create Team" });
   }
