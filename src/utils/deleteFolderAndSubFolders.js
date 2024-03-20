c2ed7e758b7c83bf79fb83e841ca68949d969c1c
JungDeunGyul <ask7384@gmail.com>
2024-03-21 04:03:35 +0900

    Feat: implement utility function for deleting folders and their subfolders

---
+++ src/utils/deleteFolderAndSubFolders.js
@@ -0,0 +1,32 @@
+const { Folder } = require("../models/Folder");
+const { File } = require("../models/File");
+
+async function deleteFolderAndSubFolders(folderId) {
+  try {
+    const folder = await Folder.findById(folderId.toString());
+
+    if (!folder) {
+      throw new Error("폴더를 찾을 수 없습니다.");
+    }
+
+    const subFolders = folder.subFolders;
+
+    for (const subFolderId of subFolders) {
+      await deleteFolderAndSubFolders(subFolderId);
+    }
+
+    for (const fileId of folder.files) {
+      const file = await File.findById(fileId);
+      if (file) {
+        await file.deleteOne();
+      }
+    }
+
+    await folder.deleteOne();
+  } catch (error) {
+    console.error(`폴더 및 하위 폴더 삭제 중 오류 발생: ${error.message}`);
+    throw error;
+  }
+}
+
+module.exports = deleteFolderAndSubFolders;
