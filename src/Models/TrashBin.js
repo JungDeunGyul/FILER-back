6103bfa71263e2d099269254649f45391305e66d
JungDeunGyul <ask7384@gmail.com>
2024-03-20 17:34:33 +0900

    Refactor: trashBinSchema to use single trashItemSchema for both files and folders

--- src/Models/TrashBin.js
+++ src/Models/TrashBin.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const trashBinSchema = new Schema({
-  itemType: { type: String, enum: ["File", "Folder"], required: true },
-  item: { type: Schema.Types.ObjectId, refPath: "itemType", required: true },
+const trashItemSchema = new Schema({
+  item: { type: Schema.Types.ObjectId, required: true, refPath: "itemType" },
+  itemType: { type: String, required: true, enum: ["Folder", "File"] },
   deleted_at: {
     type: Date,
     default: () => Date.now() + 30 * 24 * 60 * 60 * 1000,
   },
+});
+
+const trashBinSchema = new Schema({
+  ownerTeam: { type: Schema.Types.ObjectId, ref: "Team", required: true },
+  folders: [trashItemSchema],
+  files: [trashItemSchema],
 });
 
 exports.TrashBin = mongoose.model("TrashBin", trashBinSchema);
