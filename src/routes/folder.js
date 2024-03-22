38139816f6d7844dc19c050529fc27301b262b35
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-03-22 16:09:24 +0900

    feat: folder permission authority

--- src/routes/folder.js
+++ src/routes/folder.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { User } = require("../models/User");
 const { Folder } = require("../models/Folder");
 const { Team } = require("../models/Team");
 
@@ -73,6 +74,60 @@ router.post("/:folderId/new", async (req, res) => {
     res.status(201).json({ message: "Folder created successfully", folder });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.patch("/permission/:folderId", async (req, res, next) => {
+  try {
+    const { folderId } = req.params;
+    const { currentUserRole, selectedRole, userId } = req.body;
+
+    const folder = await Folder.findById(folderId);
+
+    if (currentUserRole !== "팀장") {
+      return res
+        .status(403)
+        .json({ message: "당신은 권한 설정에 대한 권한이 없습니다" });
+    }
+
+    if (!folder) {
+      return res.status(412).json({ message: "폴더가 존재하지 않습니다" });
+    }
+
+    folder.visibleTo = selectedRole;
+
+    await folder.save();
+
+    const user = await User.findById(userId)
+      .populate({
+        path: "teams",
+        populate: [
+          {
+            path: "members.user",
+          },
+          {
+            path: "ownedFolders",
+          },
+          {
+            path: "ownedFiles",
+          },
+          {
+            path: "joinRequests.user",
+          },
+        ],
+      })
+      .populate({
+        path: "notifications",
+        populate: {
+          path: "team",
+        },
+      });
+
+    res
+      .status(201)
+      .json({ message: "폴더 권한이 성공적으로 변경되었습니다", user });
+  } catch (error) {
+    res.status(404).json({ error: "폴더 권한 설정에 문제가 생겼습니다" });
   }
 });
 
