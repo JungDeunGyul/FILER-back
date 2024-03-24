2320aa89f45fc0c4c306036bf2bafffebaac0a32
JungDeunGyul <ask7384@gmail.com>
2024-03-25 04:01:04 +0900

    Feat: update member role in team management endpoint

--- src/routes/team.js
+++ src/routes/team.js
@@ -40,7 +40,7 @@ router.post("/:teamName/createfolder/:userId", async (req, res, next) => {
     const { userId, teamName } = req.params;
     const { folderName } = req.body;
 
-    const user = await User.findOne({ _id: userId });
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -605,6 +605,208 @@ router.delete("/:teamName/withdraw/:userId", async (req, res, next) => {
   return res
     .status(200)
     .json({ message: `팀 ${teamName}에서 탈퇴 되었습니다.`, updatedUser });
+});
+
+router.patch("/:selectedMemberId/manageteam/", async (req, res, next) => {
+  try {
+    const { selectedMemberId } = req.params;
+    const { currentUserRole, selectedRole, teamId, userId } = req.body;
+
+    if (currentUserRole !== "팀장") {
+      return res
+        .status(201)
+        .json({ message: "당신은 팀 관리 권한이 없습니다" });
+    }
+
+    const team = await Team.findById(teamId).populate({
+      path: "members",
+    });
+
+    const targetUserData = await User.findById(selectedMemberId)
+      .populate({
+        path: "teams",
+        populate: [
+          {
+            path: "members.user",
+          },
+          {
+            path: "ownedFolders",
+          },
+          {
+            path: "ownedFiles",
+            populate: {
+              path: "versions",
+              populate: {
+                path: "file",
+              },
+            },
+          },
+          {
+            path: "joinRequests.user",
+          },
+        ],
+      })
+      .populate({
+        path: "notifications",
+        populate: {
+          path: "team",
+        },
+      });
+
+    if (selectedRole === "팀장") {
+      const teamLeaderId = team.leader.toString();
+
+      team.leader = selectedMemberId;
+
+      const selectedMemberToTeamLeader = team.members.find(
+        (member) => member.user.toString() === selectedMemberId,
+      );
+      selectedMemberToTeamLeader.role = selectedRole;
+
+      const teamLeaderToTeamMember = team.members.find(
+        (member) => member.user.toString() === teamLeaderId,
+      );
+      teamLeaderToTeamMember.role = "팀원";
+
+      await team.save();
+
+      const teamToUpdate = targetUserData.teams.find(
+        (team) => team._id.toString() === teamId,
+      );
+
+      if (!teamToUpdate) {
+        return res
+          .status(404)
+          .json({ message: "선택된 팀을 찾을 수 없습니다" });
+      }
+
+      const memberToUpdate = teamToUpdate.members.find(
+        (member) => member.user._id.toString() === selectedMemberId,
+      );
+
+      const currentTeamLeaderToUpdate = teamToUpdate.members.find(
+        (member) => member.user._id.toString() === teamLeaderId,
+      );
+
+      if (!memberToUpdate) {
+        return res
+          .status(404)
+          .json({ message: "선택된 멤버를 팀에서 찾을 수 없습니다" });
+      }
+
+      memberToUpdate.role = selectedRole;
+      currentTeamLeaderToUpdate.role = "팀원";
+
+      await targetUserData.save();
+
+      sendUserDataToClients(targetUserData, selectedMemberId);
+
+      const currentUser = await User.findById(userId)
+        .populate({
+          path: "teams",
+          populate: [
+            {
+              path: "members.user",
+            },
+            {
+              path: "ownedFolders",
+            },
+            {
+              path: "ownedFiles",
+              populate: {
+                path: "versions",
+                populate: {
+                  path: "file",
+                },
+              },
+            },
+            {
+              path: "joinRequests.user",
+            },
+          ],
+        })
+        .populate({
+          path: "notifications",
+          populate: {
+            path: "team",
+          },
+        });
+
+      return res.status(200).json({
+        message: "멤버의 권한이 성공적으로 변경되었습니다",
+        currentUser,
+      });
+    }
+
+    const member = team.members.find(
+      (member) => member.user.toString() === selectedMemberId,
+    );
+
+    if (!member) {
+      return res
+        .status(404)
+        .json({ message: "선택된 멤버를 팀에서 찾을 수 없습니다" });
+    }
+
+    member.role = selectedRole;
+
+    const teamToUpdate = targetUserData.teams.find(
+      (team) => team._id.toString() === teamId,
+    );
+
+    if (!teamToUpdate) {
+      return res.status(404).json({ message: "선택된 팀을 찾을 수 없습니다" });
+    }
+
+    const memberToUpdate = teamToUpdate.members.find(
+      (member) => member.user._id.toString() === selectedMemberId,
+    );
+
+    memberToUpdate.role = selectedRole;
+
+    await team.save();
+    await targetUserData.save();
+
+    sendUserDataToClients(targetUserData, selectedMemberId);
+
+    const currentUser = await User.findById(userId)
+      .populate({
+        path: "teams",
+        populate: [
+          {
+            path: "members.user",
+          },
+          {
+            path: "ownedFolders",
+          },
+          {
+            path: "ownedFiles",
+            populate: {
+              path: "versions",
+              populate: {
+                path: "file",
+              },
+            },
+          },
+          {
+            path: "joinRequests.user",
+          },
+        ],
+      })
+      .populate({
+        path: "notifications",
+        populate: {
+          path: "team",
+        },
+      });
+
+    return res.status(200).json({
+      message: "멤버의 권한이 성공적으로 변경되었습니다",
+      currentUser,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 router.get("/filer-stream/:loginUserId", (req, res) => {
