8a3e3cf5e13ceb2837089cb67056e44114bdcb25
JungDeunGyul <ask7384@gmail.com>
2024-11-10 01:46:12 +0900

    Feat: move file creation process to a utility function for reuse

---
+++ src/utils/createFile.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+const { File } = require(path.resolve(__dirname, "../Models/File"));
+
+const createFile = async (
+  decodedFileName,
+  uploadedFile,
+  teamId,
+  userNickname,
+) => {
+  const newFile = await File.create({
+    name: decodedFileName,
+    size: uploadedFile.size,
+    type: uploadedFile.mimetype,
+    ownerTeam: teamId,
+    uploadUser: userNickname,
+    filePath: uploadedFile.location,
+    s3Key: uploadedFile.key,
+  });
+
+  newFile.versions.push({
+    versionNumber: 1,
+    file: newFile._id,
+  });
+
+  await newFile.save();
+
+  return newFile;
+};
+
+module.exports = {
+  createFile,
+};
