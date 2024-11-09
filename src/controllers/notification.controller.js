a4f8c2222a766ed4e7dbcbcac7dcf86e3a63a768
JungDeunGyul <ask7384@gmail.com>
2024-11-10 00:52:24 +0900

    Refactor: split notificationPatch logic into controller

---
+++ src/controllers/notification.controller.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const { User } = require(path.resolve(__dirname, "../Models/User"));
+
+const { populateUserDetails } = require(
+  path.resolve(__dirname, "../utils/populateHelpers"),
+);
+
+const notificationPatch = async (req, res, next) => {
+  try {
+    const { notificationId } = req.params;
+
+    const user = await User.findOneAndUpdate(
+      { "notifications._id": notificationId },
+      { $set: { "notifications.$.isRead": true } },
+      { new: true },
+    ).populate(populateUserDetails());
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User or notification not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Notification marked as read successfully", user });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  notificationPatch,
+};
--- src/routes/notification.js
+++ src/routes/notification.js
@@ -2,75 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-const { User } = require(path.resolve(__dirname, "../Models/User"));
+const { notificationPatch } = require(
+  path.resolve(__dirname, "../controllers/notification.controller"),
+);
 
-router.patch("/:notificationId", async (req, res) => {
-  try {
-    const { notificationId } = req.params;
-
-    const user = await User.findOneAndUpdate(
-      { "notifications._id": notificationId },
-      { $set: { "notifications.$.isRead": true } },
-      { new: true },
-    )
-      .populate({
-        path: "teams",
-        populate: [
-          {
-            path: "members.user",
-          },
-          {
-            path: "ownedFolders",
-          },
-          {
-            path: "ownedFiles",
-            populate: {
-              path: "versions",
-              populate: {
-                path: "file",
-              },
-            },
-          },
-          {
-            path: "ownedFiles",
-            populate: {
-              path: "versions",
-              populate: {
-                path: "file",
-                populate: {
-                  path: "comments",
-                  populate: {
-                    path: "user",
-                  },
-                },
-              },
-            },
-          },
-          {
-            path: "joinRequests.user",
-          },
-        ],
-      })
-      .populate({
-        path: "notifications",
-        populate: {
-          path: "team",
-        },
-      });
-
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: "User or notification not found" });
-    }
-
-    res
-      .status(200)
-      .json({ message: "Notification marked as read successfully", user });
-  } catch (error) {
-    console.error("Error marking notification as read:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.patch("/:notificationId", notificationPatch);
 
 module.exports = router;
