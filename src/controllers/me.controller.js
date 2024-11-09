18cc7abc30671aa24dfb53f8954a9816dff7db38
JungDeunGyul <ask7384@gmail.com>
2024-11-09 19:42:14 +0900

    Refactor: split user retrieval logic into controller

---
+++ src/controllers/me.controller.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const { User } = require(path.resolve(__dirname, "../Models/User"));
+
+const { populateUserDetails } = require(
+  path.resolve(__dirname, "../utils/populateHelpers"),
+);
+
+const getUser = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findById(userId).populate(populateUserDetails());
+
+    if (!user) {
+      return res.status(404).json({ message: "해당 유저를 찾을 수 없습니다." });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    return res.status(500).json({ message: "유저 정보 전달 실패" });
+  }
+};
+
+module.exports = {
+  getUser,
+};
